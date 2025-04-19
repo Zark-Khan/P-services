1d5e2bddd0b9b1812af2ff2e7fa30fe30b4eda63
Zark-Khan <zarkkhan17@gmail.com>
2025-04-19 15:39:59 +0500

    strapi content

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
   // Fetch the data dynamically from the API
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://essential-fun-2e6b6df39c.strapiapp.com/api/home-page';
-      const token = '8a24c5ea77a5327df59a231af99f58a27bdfcad028e3bc9ebd70d2a26a278bf6af84d03d7adcfc0cc1505f1d0af09712cd9b22f4b0505ad2092e929e79d67e18483e9ffbc3dd19a4d95924ee8763b81ef566119b4db7d892fed36b82dcb270d74a6a99450e10c95d82d2cd9bb4cb8a774721b155538800bc570bb862211f2ea1';
+      const url = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/home-page`;
+      const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
       try {
         const response = await fetch(url, {
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import Head from 'next/head';
 import HomeCompo from './components/Home';
 
 export default async function Home() {
-  const url = 'https://essential-fun-2e6b6df39c.strapiapp.com/api/home-page?populate[list_values][populate]=image_list';
-  const token = '8a24c5ea77a5327df59a231af99f58a27bdfcad028e3bc9ebd70d2a26a278bf6af84d03d7adcfc0cc1505f1d0af09712cd9b22f4b0505ad2092e929e79d67e18483e9ffbc3dd19a4d95924ee8763b81ef566119b4db7d892fed36b82dcb270d74a6a99450e10c95d82d2cd9bb4cb8a774721b155538800bc570bb862211f2ea1';
+  const url = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/home-page?populate[list_values][populate]=image_list`;
+  const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
+
 
   try {
     const response = await fetch(url, {
